11adb354b16f7c918725a6992921b7ffd44a2286
Chris Hutchinson <chris_hutchinson@me.com>
2018-11-13 12:23:09 +0000

    Add initial test harness

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,9 @@
+import { largeRem } from "../index";
+
+describe("large-rem-js", () => {
+  it("should not change a set integers already equal to 100", () => {
+    expect(largeRem([20, 60, 20])).toEqual(
+      expect.arrayContaining([20, 60, 20])
+    );
+  });
+});
