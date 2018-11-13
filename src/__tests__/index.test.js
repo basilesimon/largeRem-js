1fcbede2641796b846b32365d11583c1f08e2e20
Chris Hutchinson <chris_hutchinson@me.com>
2018-11-13 12:26:31 +0000

    Fix typo

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import { largeRem } from "../index";
 
 describe("large-rem-js", () => {
-  it("should not change a set integers already equal to 100", () => {
+  it("should not change a set of integers already equal to 100", () => {
     expect(largeRem([20, 60, 20])).toEqual(
       expect.arrayContaining([20, 60, 20])
     );
